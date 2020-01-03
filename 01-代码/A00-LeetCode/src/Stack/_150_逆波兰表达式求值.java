8ad2b133d3280ef784b1c58ea4000698d9db2518
goodswifter <1042480866@qq.com>
2020-01-03 21:46:26 +0800

    1

--- 01-代码/A00-LeetCode/src/Stack/_150_逆波兰表达式求值.java
+++ 01-代码/A00-LeetCode/src/Stack/_150_逆波兰表达式求值.java
@@ -1,7 +1,5 @@
 package Stack;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Stack;
 
 /**
@@ -11,15 +9,61 @@ import java.util.Stack;
  *
  */
 public class _150_逆波兰表达式求值 {
-    public int evalRPN(String[] tokens) {
+	
+	public static void main(String[] args) {
+		String[] tokens = {"2", "1", "-", "3", "*"};
+ 		System.out.println(evalRPN(tokens));
+	}
+	
+	public static int evalRPN(String[] tokens) {
+		Stack<Integer> stack = new Stack<>();
+        
+        for (String token : tokens) {
+        	switch (token) {
+				case "+" : stack.push(stack.pop() + stack.pop()); break;
+				case "-" : {
+		        	int right = stack.pop();
+		        	int left = stack.pop();
+					stack.push(left - right);
+					break;
+				}
+				case "*" : stack.push(stack.pop() * stack.pop()); break;
+				case "/" : {
+		        	int right = stack.pop();
+		        	int left = stack.pop();
+		        	stack.push(left / right);
+		        	break;
+				}
+				default  : stack.push(Integer.parseInt(token));
+			}
+        }
+		
+		return stack.pop();
+    }
+	
+	public static int evalRPN2(String[] tokens) {
+		Stack<Integer> stack = new Stack<>();
+        
+        for (String item : tokens) {
+        	stack.push(isOperator(item) 
+        			? stack.push(calculate(item, stack.pop(), stack.pop()))
+        			: stack.push(Integer.parseInt(item)));
+        }
+		
+		return stack.pop();
+    }
+	
+    public int evalRPN1(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         
         for (String item : tokens) {
 			if (isOperator(item)) {
-				int a = stack.pop();
-				int b = stack.pop();
-		
-				stack.push(calculate(item, a, b));
+				// 要先取出右操作数, 再取出左操作数
+//				int right = stack.pop();
+//				int left = stack.pop();
+//		
+//				stack.push(calculate(item, left, right));
+				stack.push(calculate(item, stack.pop(), stack.pop()));
 			} else {
 				stack.push(Integer.parseInt(item));
 			}
@@ -32,27 +76,27 @@ public class _150_逆波兰表达式求值 {
      * 计算两个数的值
      * 
      * @param operator 运算符
-     * @param a 第一个数
-     * @param b 第二个数
+     * @param left 左操作数
+     * @param right 右操作数
      * @return 结果
      */
-    private int calculate(String operator, int a, int b) {
+    private static int calculate(String operator, int right, int left) {
 		int result = 0;
 		switch (operator) {
 		case "+":
-			result = (a + b);
+			result = (left + right);
 			break;
 			
 		case "-":
-			result = (a - b);
+			result = (left - right);
 			break;
 			
 		case "*":
-			result = (a * b);
+			result = (left * right);
 			break;
 			
 		case "/":
-			result = (a / b);
+			result = (left / right);
 			break;
 		}
 		
@@ -62,14 +106,7 @@ public class _150_逆波兰表达式求值 {
     /**
      * 是不是运算符
      */
-    private boolean isOperator(String str) {
-    	
-    	Set<String> set = new HashSet<>();
-    	set.add("+");
-    	set.add("-");
-    	set.add("*");
-    	set.add("/");
-    	
-		return set.contains(str);
+    private static boolean isOperator(String str) {
+    	return "+-*/".contains(str);
 	}
 }
